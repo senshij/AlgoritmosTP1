4b542ba0277eaf463a4a4461f2d190d8feb6efff
Lucas Ovalle <luke.ovalle@gmail.com>
2017-05-24 18:24:11 -0300

    stuff

--- config.h
+++ config.h
@@ -1,12 +1,18 @@
+#include <stdio.h>
+
+typedef enum{
+    GREGORIAN_FORMAT,
+    JULIAN_FORMAT,
+} format_t;
+
 typedef struct{
     format_t format
 } config_t;
 
 typedef enum{
-    ERROR,
     OK,
-    ERROR_READ,
-    END_OF_FILE,
+    ERROR_INVALID_ARGS,
+    ERROR_EOF,
+    ERROR_INVALID_DATA,
+    ERROR_NULL_POINTER,
 } status_t;
-
-status_t validate_args(int argc, char *argv[], *config_t);
